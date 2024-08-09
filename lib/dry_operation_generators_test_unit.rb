55bee763df9761a66e697fd643a53010439903fe
Joel AZEMAR <joel.azemar@gmail.com>
2024-08-09 11:38:13 +0200

    Initial Commit

---
+++ lib/dry_operation_generators_test_unit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dry_operation_generators_test_unit/version"
+
+module DryOperationGeneratorsTestUnit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dry_operation_generators_test_unit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DryOperationGeneratorsTestUnit
+  VERSION = "0.1.0"
+end
